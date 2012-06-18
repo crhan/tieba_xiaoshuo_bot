d740e8f232b21403ab51c8a1e0c397769c2fe6d4
crhan <crhan123@gmail.com>
2012-06-18 20:21:36 +0800

    add subsctiption model autogenerate fiction URL by encode GBK name

--- lib/model/fictions.rb
+++ lib/model/fictions.rb
@@ -1,4 +1,21 @@
+require "uri"
 class Fiction < Sequel::Model
+  # create_table? :fictions do
+  #   primary_key :id
+  #   index :id
+  #   String :name
+  #   String :url
+  # end
   many_to_many :users, :join_table => :subscriptions
   one_to_many :check_lists
+  plugin :validation_helpers
+
+  def validate
+    super
+    validates_presence :name
+  end
+
+  def before_save
+    self.url = URI.encode name.encode("gbk")
+  end
 end
