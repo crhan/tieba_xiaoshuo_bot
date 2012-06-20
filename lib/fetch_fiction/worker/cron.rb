a3db2021bf4f2dab87b4ca7a705dce21451d4f82
crhan <crhan123@gmail.com>
2012-06-20 22:50:55 +0800

    add namespace to worker

--- lib/fetch_fiction/worker/cron.rb
+++ lib/fetch_fiction/worker/cron.rb
@@ -1,12 +1,14 @@
 # coding: utf-8
-class CronFetch
-  include Sidekiq::Worker
-  sidekiq_options :retry => false
-  sidekiq_options :queue => :cronJob
+module FetchFiction
+  class CronFetch
+    include Sidekiq::Worker
+    sidekiq_options :retry => false
+    sidekiq_options :queue => :cronJob
 
-  def perform
-    Fiction.each_entry do |e|
-      Fetch.perform_async e.id
+    def perform
+      Fiction.each_entry do |e|
+        Fetch.perform_async e.id
+      end
     end
   end
 end
