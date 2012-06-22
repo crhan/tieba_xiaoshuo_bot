c6c5345ac9fe18567499f94c6dd956a38cad9ac6
crhan <crhan123@gmail.com>
2012-06-22 23:52:16 +0800

    can now receive command "-unsub fiction_name" from gtalk

--- sidekiq.rb
+++ sidekiq.rb
@@ -2,10 +2,10 @@
 require 'bundler/setup'
 require './lib/fetch_fiction/rexml_utf_patch'
 require './lib/fetch_fiction'
-require './lib/fetch_fiction/bot'
 Dir.glob './**/worker/*.rb' do |f|
   require f
 end
+require './lib/fetch_fiction/bot'
 include FetchFiction
 $bot = Bot.instance
 
@@ -14,4 +14,6 @@ $logger.debug Worker::OnlineCheck.sidekiq_options
 $logger.debug Worker::Send.sidekiq_options
 $logger.debug Worker::Fetch.sidekiq_options
 $logger.debug Worker::LogError.sidekiq_options
-Jabber::debug = true
+$logger.debug Worker::Sub.sidekiq_options
+$logger.debug Worker::UnSub.sidekiq_options
+#Jabber::debug = true
