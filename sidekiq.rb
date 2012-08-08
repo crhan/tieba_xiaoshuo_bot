4cfed21cbb998519e67db0bbae6d641c161633fd
crhan <crhan123@gmail.com>
2012-08-08 18:41:37 +0800

    分离 测试和线上环境的数据库配置

--- sidekiq.rb
+++ sidekiq.rb
@@ -1,17 +1,9 @@
 # coding: utf-8
 $:.unshift File.expand_path("../lib", __FILE__)
 require 'bundler/setup'
+CONFIG = YAML.load_file("config/config.yml")["production"]
 require 'tieba_xiaoshuo_bot'
 
-include TiebaXiaoshuoBot
-gtalk = YAML.load_file("config/config.yml")["gtalk"]
-$bot = Bot.new gtalk
+$bot = TiebaXiaoshuoBot::Bot.new CONFIG["gtalk"]
 
-$logger.debug Worker::CronFetch.sidekiq_options
-$logger.debug Worker::OnlineCheck.sidekiq_options
-$logger.debug Worker::Send.sidekiq_options
-$logger.debug Worker::Fetch.sidekiq_options
-$logger.debug Worker::LogError.sidekiq_options
-$logger.debug Worker::Sub.sidekiq_options
-$logger.debug Worker::UnSub.sidekiq_options
 Jabber::debug = true
