e4c1bfe6464f5d4e936b59e517f01e5b054234ac
crhan <crhan123@gmail.com>
2012-09-02 22:43:03 +0800

    修正无法获得小说贴吧列表的问题  - 来自百度贴吧的奇怪更新, 如果在请求头中没有附带 BAIDUID 的 cookie 的话会导致 301 跳转

--- lib/tieba_xiaoshuo_bot/model/fictions.rb
+++ lib/tieba_xiaoshuo_bot/model/fictions.rb
@@ -7,6 +7,8 @@ module TiebaXiaoshuoBot
     class HTTPResponseError < StandardError; end
     class NoContentError < StandardError; end
     class ChapterExistError < StandardError; end
+    class HTTPMovedPermanentlyError< StandardError; end
+
     plugin :validation_helpers
     plugin :schema
     many_to_many :users, :join_table => :subscriptions
@@ -20,7 +22,6 @@ module TiebaXiaoshuoBot
     # String :encode_url
     # Time :created_at
     # Time :updated_at
-
 
     def subscriptions
       Subscription.filter(:fiction => self)
@@ -61,23 +62,47 @@ module TiebaXiaoshuoBot
       self.encode_url = URI.encode name.encode("gbk")
     end
 
+    def cookies resp
+      resp
+        .get_fields('set-cookie')
+        .inject(Array.new) { |a,i|
+          a.push(i.split('; ').first)
+        }
+        .join('; ');
+    end
+
     def get_response
       # fetch resource, retry 3 times
       begin
-        retry_time ||= 0
-        body = Net::HTTP.get_response(URI.parse(self.url)).body.encode("utf-8", "gbk")
-        body
+        uri = URI(self.url)
+        http = Net::HTTP.new(uri.host)
+        if !!@@cookies ||= nil
+          resp = http.get(%|#{uri.path}?#{uri.query}|, { "Cookie" => @@cookies })
+        else
+          resp = http.get("#{uri.path}?#{uri.query}")
+        end
+
+        if resp.is_a?(Net::HTTPMovedPermanently)
+          @@cookies = cookies(resp)
+          raise HTTPMovedPermanentlyError, %|Get cookie: "#{@@cookies}"|
+        else
+          resp.body.encode("utf-8", "gbk")
+        end
+      rescue HTTPMovedPermanentlyError => e
+        retry
       rescue => e
+        retry_time ||= 0
         if retry_time< 3
-          $logger.error e.inspect
-          sleep retry_time ** 4 + 5
+          $logger.error %|#{e.inspect}, #{__LINE__}|
           retry_time += 1
-          $logger.warn %|Retry #{times} time(s)|
+          $logger.warn %|Retry #{retry_time} time(s)|
+          sleep retry_time ** 4 + 5
           retry
         end
         raise HTTPResponseError, "三次 #{self.name}#get_response 均失败了"
       end
     end
+
 
     def new_chapters body=get_response
       result = Nokogiri::HTML(body)
