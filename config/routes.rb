20a6fe2957c7e1b6288a6f1433cf4d3252b179ba
@ruohanc <crhan123@gmail.com>
2012-10-20 19:45:13 +0800

    monitor sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 XiaoshuoBot::Application.routes.draw do
+  require "sidekiq/web"
+  mount Sidekiq::Web => "/sidekiq"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
