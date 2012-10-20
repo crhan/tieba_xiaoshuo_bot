87448fe98c11e1f32a9116852c00195d1ca885a2
@ruohanc <crhan123@gmail.com>
2012-10-20 15:12:56 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XiaoshuoBot::Application.config.secret_token = '7afd2e8eb327ada60032764f9810ce6b1fc55cd476e9d4c8a1a66006e277f90ea614e825c89b2b5e543b229cf64f200cef2610e4fc7a760fa8d3aade2e0b50b2'
