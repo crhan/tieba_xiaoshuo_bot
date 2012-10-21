127e4a431879f02077d866d3c9d7f989a400f32d
@ruohanc <crhan123@gmail.com>
2012-10-21 20:03:30 +0800

    test change

--- test/unit/subscription_test.rb
+++ test/unit/subscription_test.rb
@@ -2,7 +2,9 @@
 require 'test_helper'
 
 class SubscriptionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "shoud_not subscribe same fiction again" do
+    assert_raise(ActiveRecord::RecordInvalid) do
+      Subscription.create!(user: User.first, fiction:Fiction.first)
+    end
+  end
 end
