4f3e7444d567ce1d2b97b8e30be3e406531cf1ee
@ruohanc <crhan123@gmail.com>
2012-10-21 17:09:54 +0800

    add chapter create trigger and test

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,13 +1,18 @@
 # -*- encoding : utf-8 -*-
 class Chapter < ActiveRecord::Base
-  belongs_to :fiction
   attr_accessible :title, :thread_id, :fiction, :active
+  before_create :deactive_same_fiction_title
+  belongs_to :fiction
   validates :title, :thread_id, :presence => true
   validates :thread_id, :uniqueness => true
-  before_create :deactive_same_fiction_title
+  default_scope where(active: true)
+
+  def to_s
+    "#{self.title}, http://wapp.baidu.com/m?kz=#{thread_id}"
+  end
 
   protected
   def deactive_same_fiction_title
-    Chapter.where(active: true, title: self.title).update_all(active: false)
+    Chapter.where(title: self.title, fiction_id: self.fiction.id).update_all(active: false)
   end
 end
--- test/unit/chapter_test.rb
+++ test/unit/chapter_test.rb
@@ -5,4 +5,11 @@ class ChapterTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should deactive the same chapter title in one fiction" do
+    Chapter.create(fiction: Fiction.find(1), title: "第二章", thread_id: 192411234)
+    chapter_ar = Chapter.unscoped.where(title: "第二章", active: true)
+    assert_equal 2, chapter_ar.count
+    assert_equal 192411234, chapter_ar.where(fiction_id: 1).first.thread_id
+    assert !Chapter.find_by_thread_id(192311233).active
+  end
 end
