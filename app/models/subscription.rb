5c74452538097b9614f23b496dccd607f45b941a
@ruohanc <crhan123@gmail.com>
2012-12-13 12:20:37 +0800

    bug fix agina

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -9,6 +9,7 @@ class Subscription < ActiveRecord::Base
   validates :fiction_id, :uniqueness => { :scope => :user_id }
   delegate :name, :to => :fiction, :prefix => true
   delegate :chapters, :to => :fiction
+  delegate :update_chapters, :to => :fiction
   scope :active_fictions, where(active: true).group(:fiction_id)
   scope :active, where(active: true)
 
